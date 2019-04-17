2f59e5940e737bbed75107da7936fa6f7c38fe30
Justin Teeter <jteeter2007@gmail.com>
2019-04-16 20:04:54 -0700

    Create Animal.java

---
+++ Mid-Term/Animal.java
@@ -0,0 +1,13 @@
+package javaapplication1;
+
+public class Animal {
+    private String name = "";
+    
+    // Name Get and Set
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }  
+}
